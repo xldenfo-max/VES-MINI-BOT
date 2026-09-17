3aaa671393a0bcf782bd2f968709878c0659ea44
xldenfo-max <xldenfo@gmail.com>
2026-09-17 17:17:53 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=d419705fa0ecc02f",
+SESSION_NAME: process.env.SESSION_NAME || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/80d5w9.png",
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
+WELCOME: process.env.WELCOME || "false",
+GOODBYE: process.env.GOODBYE || "false",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_BAD: process.env.ANTI_BAD || "false",
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
+PREFIX: process.env.PREFIX || ".",
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+AUTO_REACT: process.env.AUTO_REACT || "false",
+MODE: process.env.MODE || "public",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+READ_CMD: process.env.READ_CMD || "false",
+DEV: process.env.DEV || "94740544995",
+ANTI_VV: process.env.ANTI_VV || "true",
+ANTI_DELETE: process.env.ANTI_DELETE || "true",
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox",     
+};
