9b9e23b956e058c334357d3f74977a3c9bf602e1
xldenfo-max <xldenfo@gmail.com>
2026-09-20 18:50:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ const {
 if (!fs.existsSync(__dirname + '/sessions/creds.json')) {
     if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
     
-    const sessdata = config.SESSION_ID.replace("PRABATH-MD~", '');
+    const sessdata = config.SESSION_ID.replace("KnightBot!", '');
     try {
         // Decode base64 string
         const decodedData = Buffer.from(sessdata, 'base64').toString('utf-8');
